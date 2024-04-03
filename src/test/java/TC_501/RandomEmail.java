a056a4d0a20dc5c20dc7fd3a7882ab933aebf394
ozgurrkarakus <ozgurkarakus.fb@gmail.com>
2024-04-03 23:14:25 +0300

    TC_501, TC_502 finished

---
+++ src/test/java/TC_501/RandomEmail.java
@@ -0,0 +1,32 @@
+package TC_501;
+
+import java.util.Random;
+
+public class RandomEmail {
+    public static  String[] domains = {"gmail.com"};
+    public static  int maxLength = 15;
+
+
+    public void generateRandomEmailTest() {
+        String randomEmail = generateRandomEmail();
+        System.out.println("Random Email: " + randomEmail);
+    }
+
+    public static String generateRandomEmail() {
+        Random random = new Random();
+        StringBuilder email = new StringBuilder();
+
+
+        int length = random.nextInt(maxLength) + 1;
+        for (int i = 0; i < length; i++) {
+            char c = (char) (random.nextInt(26) + 'a');
+            email.append(c);
+        }
+
+        email.append("@").append(domains[random.nextInt(domains.length)]);
+
+        return email.toString();
+    }
+
+}
+
