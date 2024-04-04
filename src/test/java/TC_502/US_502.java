f5279745d95f6601f1b6d70ff332c77d5e5e9a80
Özgür <ozgurkarakus.fb@gmail.com>
2024-04-04 22:52:07 +0300

    Update US_502.java

--- src/test/java/TC_502/US_502.java
+++ src/test/java/TC_502/US_502.java
@@ -1,12 +1,25 @@
 package TC_502;
 
+import TC_501.TC_501_502_Elements;
 import Utility.BaseDriver;
+import org.testng.Assert;
 import org.testng.annotations.Test;
 
 public class US_502 extends BaseDriver {
     @Test
-    public void Login(){
-        LoginTest();
+
+    public void Login() {
+        TC_501_502_Elements element = new TC_501_502_Elements();
+        driver.get("https://demo.nopcommerce.com/");
+        element.login.click();
+//String randomEmail = RandomEmail.generateRandomEmail();
+
+        element.loginEmail.sendKeys("ozgurkarakus.fb@gmail.com");
+        element.loginPassword.sendKeys("Admin123");
+        element.loginButton.click();
+
+        Assert.assertEquals(element.logOut.getText(), "Log out","Error");
+
 
     }
 }
